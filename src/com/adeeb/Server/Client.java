22cd232fdc7964e73824d41956631b40953d1e43
AdeebNqo <adeebnqo@gmail.com>
2013-04-03 22:40:37 +0200

    starting client to server communication afresh

--- src/com/adeeb/Server/Client.java
+++ src/com/adeeb/Server/Client.java
@@ -1,24 +1,12 @@
 package com.adeeb.Server;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.Socket;
 
+import java.net.Socket;
 public class Client {
 	public static void main(String[] args) {
 		try {
 			Socket client_socket = new Socket("localhost", 9998);
-			BufferedReader input = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-			String line="";
-			while((line=input.readLine())!=null){
-				System.out.println(line);
-			}
-			input.close();
-			
-			//Reading data from user via console
-			BufferedReader user_input = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("You've just entered "+(user_input.readLine()));
-			
-			client_socket.close();
+			//do something
+			if (!client_socket.isClosed()) client_socket.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/com/adeeb/Server/Server.java
+++ src/com/adeeb/Server/Server.java
@@ -1,8 +1,5 @@
 package com.adeeb.Server;
 
-import java.io.BufferedWriter;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,12 +10,10 @@ public class Server {
 		try {
 			data_link = new ServerSocket(9998);
 			while (true) {
-				System.err
-						.println("Server running...\nWaiting for client connection.");
+				System.err.println("Server running...\nWaiting for client connection.");
 				final Socket current_client = data_link.accept();
-				System.err.println("Client "
-						+ current_client.getRemoteSocketAddress().toString()
-						+ " has connected");
+				System.err.println("Client "+ current_client.getRemoteSocketAddress().toString()+ " has connected");
+				
 				Interact client_response_system = new Interact(current_client);
 				client_response_system.start();
 			}
@@ -39,33 +34,7 @@ public class Server {
 
 		public void run() {
 			// run the interaction process until the client quits
-			try {
-				System.err.println("abt to display menu");
-				display_menu(client.getOutputStream());
-				System.err.println("done displaying menu!");
-				// getting menu selection from the client
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-		}
-
-		/*
-		 * Method for displaying the server menu to a client
-		 */
-		public void display_menu(OutputStream outstream) {
-			try {
-				BufferedWriter writer = new BufferedWriter(new PrintWriter(
-						outstream));
-				writer.write("1. Display own data\n");
-				writer.write("2. Display some group's data\n");
-				writer.write("3. Display all data\n");
-				writer.write("4. Retrieve raw data(whatever that means)\n");
-				writer.write("5. disconnect\n");
-				writer.flush();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			
 		}
 	}
 }
