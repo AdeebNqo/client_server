0fa827a05f40b3a595b378bba5ba0dd7779dfc79
AdeebNqo <adeebnqo@gmail.com>
2013-04-03 23:06:41 +0200

    still debuggin'

--- src/com/adeeb/Server/Client.java
+++ src/com/adeeb/Server/Client.java
@@ -1,11 +1,33 @@
 package com.adeeb.Server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 public class Client {
 	public static void main(String[] args) {
 		try {
 			Socket client_socket = new Socket("localhost", 9998);
-			//do something
+			/*
+			 * Communication between client and server
+			 * 
+			 */
+			System.err.println("client: trying to acquire the input and output streams");
+			BufferedReader in = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+			BufferedWriter out = new BufferedWriter(new PrintWriter(client_socket.getOutputStream()));
+			System.err.println("client: done acquiring streams");
+			
+			String curr_line ="";
+			//reading menu
+			System.err.println("client: before while loop in 'reading menu'");
+			while((curr_line=in.readLine())!=null){
+				System.out.println(curr_line+"\n");
+			}
+			
+			//closing input and output stream writers
+			in.close(); out.close();
+			//if socket is still open, close it
 			if (!client_socket.isClosed()) client_socket.close();
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/com/adeeb/Server/Server.java
+++ src/com/adeeb/Server/Server.java
@@ -1,5 +1,9 @@
 package com.adeeb.Server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,6 +19,7 @@ public class Server {
 				System.err.println("Client "+ current_client.getRemoteSocketAddress().toString()+ " has connected");
 				
 				Interact client_response_system = new Interact(current_client);
+				System.err.println("server: started response system for client");
 				client_response_system.start();
 			}
 		} catch (Exception e) {
@@ -33,8 +38,27 @@ public class Server {
 		}
 
 		public void run() {
-			// run the interaction process until the client quits
-			
+			try{
+				// run the interaction process until the client quits
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				BufferedWriter out = new BufferedWriter(new PrintWriter(client.getOutputStream()));
+				System.err.println("server: before while loop of menu system");
+				while(true){
+					//provide menu
+					/*------Menu-------*/
+					out.write("1. View own group's data");
+					out.write("2. View another group's data");
+					out.write("3. View all data");
+					out.write("4. Disconnect");
+					break;
+				}
+				System.err.println("afte menu while loop");
+				//closing the input and output stream reader and writer
+				in.close(); out.close();
+				System.err.println("server: closed the input and output streams");
+			}catch(Exception e){
+				e.printStackTrace();
+			}
 		}
 	}
 }
