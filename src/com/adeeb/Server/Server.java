1eebd568bb22a0d6ce99239a038dc6c129fb07aa
AdeebNqo <adeebnqo@gmail.com>
2013-04-04 11:55:15 +0200

    before adding time stamps

--- src/com/adeeb/Server/Server.java
+++ src/com/adeeb/Server/Server.java
@@ -1,11 +1,11 @@
 package com.adeeb.Server;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
 
 public class Server {
 	static ServerSocket data_link;
@@ -14,12 +14,11 @@ public class Server {
 		try {
 			data_link = new ServerSocket(9998);
 			while (true) {
-				System.err.println("Server running...\nWaiting for client connection.");
+				System.out.println("Server running...\nWaiting for client connection.");
 				final Socket current_client = data_link.accept();
-				System.err.println("Client "+ current_client.getRemoteSocketAddress().toString()+ " has connected");
+				System.out.println("Client "+ current_client.getRemoteSocketAddress().toString()+ " has connected");
 				
 				Interact client_response_system = new Interact(current_client);
-				System.err.println("server: started response system for client");
 				client_response_system.start();
 			}
 		} catch (Exception e) {
@@ -38,26 +37,63 @@ public class Server {
 		}
 
 		public void run() {
+			// run the interaction process until the client quits
 			try{
-				// run the interaction process until the client quits
-				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				BufferedWriter out = new BufferedWriter(new PrintWriter(client.getOutputStream()));
-				System.err.println("server: before while loop of menu system");
-				while(true){
-					//provide menu
-					/*------Menu-------*/
-					out.write("1. View own group's data");
-					out.write("2. View another group's data");
-					out.write("3. View all data");
-					out.write("4. Disconnect");
-					break;
-				}
-				System.err.println("afte menu while loop");
-				//closing the input and output stream reader and writer
-				in.close(); out.close();
-				System.err.println("server: closed the input and output streams");
+				//---Display Menu---//
+				System.err.println("server: start of menu");
+				BufferedWriter writer = new BufferedWriter(new PrintWriter(client.getOutputStream()));
+				System.err.println("server: after creating writer");	
+				writer.write("1. option");
+				writer.write("2. option");
+				writer.write("3. option");
+				writer.write("4. option");
+				writer.write("5. Disconnect");
+				client.shutdownOutput();
+				System.err.println("server: end of menu");
 			}catch(Exception e){
 				e.printStackTrace();
+				System.exit(0);	
+			}
+		
+			while(true){
+				try{
+					//getting choice from user
+					BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+					System.err.println("reader?=ready: "+reader.ready());
+					String choice = reader.readLine();
+					System.err.println("reader?=ready: "+reader.ready());
+					System.err.println("choice?=null: "+(choice==null));
+					if (choice.equals("1")){
+	
+					}
+					else if (choice.equals("2")){
+
+					}
+					else if (choice.equals("3")){
+	
+					}
+					else if (choice.equals("4")){
+	
+					}
+					else if (choice.equals("5")){
+						//disconnect option
+						break;
+					}
+					else{
+	
+					}
+				}catch(Exception e){
+					e.printStackTrace();
+					System.exit(0);
+				}
+			}
+			//close socket connection
+			if (!client.isClosed()){
+				try{
+					client.close();
+				}catch(Exception e){
+					e.printStackTrace();
+				}
 			}
 		}
 	}
